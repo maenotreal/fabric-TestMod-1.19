9e2988f36b377dddee18414fffeea0abe30b5450
maenotreal <maenotreal@gmail.com>
2023-03-12 15:00:02 +0400

    init commit

---
+++ src/main/java/net/maenr/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.maenr.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
